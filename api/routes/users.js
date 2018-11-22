7d8904a9c7f774ddec82845ae66c8b08ab5b7d49
Luis Felipe <luisfelipegodoi@live.com>
2018-11-22 14:46:15 -0200

    Create a Users Route.

---
+++ api/routes/users.js
@@ -0,0 +1,94 @@
+'use strict';
+
+// Users routes
+var Joi = require('joi');
+var UsersController = require('../controllers/Users');
+
+exports.register = function(server, options, next) {
+    // Setup the controller
+    var usersController = new UsersController(options.database);
+
+    // Binds all methods
+    // similar to doing `usersController.index.bind(usersController);`
+    // when declaring handlers
+    server.bind(usersController);
+
+    // Declare routes
+    server.route([
+        {
+            method: 'GET',
+            path: '/users',
+            config: {
+                handler: usersController.index,
+                validate: {
+                    query: Joi.object().keys({
+                        start: Joi.number().min(0),
+                        limit: Joi.number().min(1)
+                    })
+                }
+            }
+        },
+        {
+            method: 'GET',
+            path: '/users/{id}',
+            config: {
+                handler: usersController.show,
+                validate: {
+                    params: {
+                        id: Joi.string().regex(/[a-zA-Z0-9]{16}/)
+                    }
+                }
+            }
+        },
+        {
+            method: 'POST',
+            path: '/users',
+            config: {
+                handler: usersController.store,
+                validate: {
+                    payload: Joi.object().length(1).keys({
+                        username: Joi.string().required().min(1).max(20),
+                        userpassword: Joi.string().required().min(1).max(50),
+                        useradmin: Joi.bool().default(false)
+                    })
+                }
+            }
+        },
+        {
+            method: 'PUT',
+            path: '/users/{id}',
+            config: {
+                handler: usersController.update,
+                validate: {
+                    params: {
+                        id: Joi.string().regex(/[a-zA-Z0-9]{16}/)
+                    },
+                    payload: Joi.object().length(1).keys({
+                        username: Joi.string().required().min(1).max(20),
+                        userpassword: Joi.string().required().min(1).max(50),
+                        useradmin: Joi.bool()
+                    })
+                }
+            }
+        },
+        {
+            method: 'DELETE',
+            path: '/users/{id}',
+            config: {
+                handler: usersController.destroy,
+                validate: {
+                    params: {
+                        id: Joi.string().regex(/[a-zA-Z0-9]{16}/)
+                    }
+                }
+            }
+        }
+    ]);
+
+    next();
+}
+
+exports.register.attributes = {
+    name: 'routes-users',
+    version: '1.0.1'
+};

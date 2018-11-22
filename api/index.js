3db0ec9189374f3a705b2a1fe4cafd19020fac7a
Luis Felipe <luisfelipegodoi@live.com>
2018-11-22 14:39:58 -0200

    Create a index.js file and define the routes of a project.

---
+++ api/index.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var Database = require('./database');
+var Hapi = require('hapi');
+
+var database = new Database();
+var server = new Hapi.Server({debug: {request: ['info', 'error']}});
+
+// Expose database
+if (process.env.NODE_ENV === 'test') {
+    server.database = database;
+}
+
+// Create server
+server.connection({
+    host: process.env.SERVER_HOST,
+    port: 8000
+});
+
+// DotEnv
+var Path = require('path');
+var DotEnv = require('dotenv');
+DotEnv.config({ path: Path.join(__dirname, '/.env'), silent: true });
+
+// Add plugins, including routes
+var plugins = [
+    {
+        register: require('./routes/users.js'),
+        options: {
+            database: database
+        }
+    },
+    {
+        register: require('./routes/products.js'),
+        options: {
+            database: database
+        }
+    },
+    {
+        register: require('./routes/stores.js'),
+        options: {
+            database: database
+        }
+    }
+];
+
+server.register(plugins, function (err) {
+    if (err) { throw err; }
+
+    if (!module.parent) {
+        server.start(function(err) {
+            if (err) { throw err; }
+
+            server.log('info', 'Server running at: ' + server.info.uri);
+        });
+    }
+});
+
+module.exports = server;

fdc51984108621327a4c347ca40e962d3179eb19
Luis Felipe <luisfelipegodoi@live.com>
2018-11-22 15:26:16 -0200

    Create a Products Model.

---
+++ api/models/Products.js
@@ -0,0 +1,97 @@
+'use strict';
+
+var crypto = require('crypto');
+
+function ProductsModel(database) {
+    this.db = database;
+};
+
+ProductsModel.prototype.getAllProducts = function() {
+    return this.db.get('products') || [];
+};
+
+ProductsModel.prototype.findProductByProperty = function(prop, value) {
+    var product, i, len;
+    var products = this.getAllProducts();
+
+    for (i = 0, len = products.length; i < len; i++) {
+        product = products[i];
+        if (product[prop] === value) {
+            return product;
+        }
+    }
+
+    return null;
+};
+
+ProductsModel.prototype.getProducts = function(start, limit) {
+    var products = this.getAllProducts();
+    return products.slice(start, limit + 1);
+};
+
+ProductsModel.prototype.getProduct = function(id) {
+    var product = this.findProductByProperty('id', id);
+
+    if (!product) {
+        throw new Error('Product doesn\'t exists.');
+    }
+
+    return product;
+};
+
+ProductsModel.prototype.addProduct = function(newProduct) {
+    var products = this.getAllProducts();
+    newProduct = newProduct.trim();
+
+    // We don't want duplicates
+    if (this.findProductByProperty('value', newProduct)) {
+        throw new Error('Product already exists for id: ' + product.id);
+    }
+
+    var product = {
+        // Collisions can happen but unlikely
+        // 1 byte to hex turns into two characters
+        id: crypto.randomBytes(8).toString('hex'),
+        value: newProduct
+    }
+    products.push(product);
+
+    this.db.set('products', products);
+
+    return product;
+};
+
+ProductsModel.prototype.updateProduct = function(id, updatedProduct) {
+    updatedProduct = updatedProduct.trim();
+
+    var product = this.findProductByProperty('id', id);
+
+    if (!product) {
+        throw new Error('Product doesn\'t exists.');
+    }
+
+    product.value = updatedProduct;
+
+    return product;
+};
+
+ProductsModel.prototype.deleteProduct = function(id) {
+    if (!this.findProductByProperty('id', id)) {
+        throw new Error('Product doesn\'t exists.');
+    }
+
+    var product, i, len;
+    var products = this.getAllProducts();
+
+    for (i = 0, len = products.length; i < len; i++) {
+        product = products[i];
+        if (product.id === id) {
+            // Removes product
+            products.splice(i, 1);
+            this.db.set('products', products);
+            return;
+        }
+    }
+};
+
+module.exports = ProductsModel;

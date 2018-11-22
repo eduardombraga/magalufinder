8d9290e80d9f242adfe2d1b298d87ed2c12f7725
Luis Felipe <luisfelipegodoi@live.com>
2018-11-22 17:25:06 -0200

    Add a getProductsStores.

--- api/models/ProductsStores.js
+++ api/models/ProductsStores.js
@@ -10,4 +10,9 @@ ProductsStoresModel.prototype.getAllProductsStores = function() {
     return this.db.get('productsstores') || [];
 };
 
+ProductsStoresModel.prototype.getProductsStores = function(start, limit) {
+    var products = this.getAllProductsStores();
+    return products.slice(start, limit + 1);
+};
+
 module.exports = ProductsStoresModel;

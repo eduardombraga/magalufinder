5fa39f62ec3354ba051f7f66a45f0d4aab4d7dbb
Luis Felipe <luisfelipegodoi@live.com>
2018-11-22 16:59:04 -0200

    Add a get all method.

--- api/controllers/ProductsStores.js
+++ api/controllers/ProductsStores.js
@@ -7,4 +7,20 @@ function ProductsStoresController(database) {
     this.productsstoresModel = new ProductsStoresModel(database);
 };
 
+// [GET] /productsstores
+ProductsStoresController.prototype.index = function(request, reply) {
+    var start = request.query.start;
+    var limit = request.query.limit;
+
+    if (start == null) {
+        start = 0
+    }
+
+    if (limit == null) {
+        limit = start + 9
+    }
+
+    reply(this.productsstoresModel.getProducts(start, limit));
+};
+
 module.exports = ProductsStoresController;

b6ee9e784428bfd77a43d5bf23b4568da7c14a68
Luis Felipe <luisfelipegodoi@live.com>
2018-11-23 10:16:52 -0200

    Create a ProductStoreRow function.

--- front/src/views/ProductsStores/ProductsStores.js
+++ front/src/views/ProductsStores/ProductsStores.js
@@ -2,3 +2,16 @@ import React, { Component } from 'react';
 import { Badge, Card, CardBody, CardHeader, Col, Row, Table } from 'reactstrap';
 
 import productsstoresData from './ProductsStoresData'
+
+function ProductStoreRow(props) {
+  const productstore = props.productstore
+  const productstoreLink = `#/productsstores/${productstore.productid}`
+
+  return (
+    <tr key={productstore.id.toString()}>
+        <th scope="row"><a href={productstoreLink}>{productstore.productid}</a></th>
+        <td><a href={productstoreLink}>{productstore.productid}</a></td>
+        <td>{productstore.storeid}</td>
+    </tr>
+  )
+}

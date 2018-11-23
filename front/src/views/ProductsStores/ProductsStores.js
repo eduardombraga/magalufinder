85b631b3ec0adf5adb965840b2b95c7ef9b42964
Luis Felipe <luisfelipegodoi@live.com>
2018-11-23 10:40:35 -0200

    Create a class component for ProductsStores.

--- front/src/views/ProductsStores/ProductsStores.js
+++ front/src/views/ProductsStores/ProductsStores.js
@@ -8,10 +8,48 @@ function ProductStoreRow(props) {
   const productstoreLink = `#/productsstores/${productstore.productid}`
 
   return (
-    <tr key={productstore.id.toString()}>
+    <tr key={productstore.productid.toString()}>
         <th scope="row"><a href={productstoreLink}>{productstore.productid}</a></th>
-        <td><a href={productstoreLink}>{productstore.productid}</a></td>
         <td>{productstore.storeid}</td>
     </tr>
   )
 }
+
+class ProductsStores extends Component {
+
+    render() {
+  
+      const productstoreList = productsstoresData.filter((productstore) => productstore.productid)
+  
+      return (
+        <div className="animated fadeIn">
+          <Row>
+            <Col xl={6}>
+              <Card>
+                <CardHeader>
+                  <i className="fa fa-align-justify"></i> Produtos x Lojas <small className="text-muted">listagem</small>
+                </CardHeader>
+                <CardBody>
+                  <Table responsive hover>
+                    <thead>
+                      <tr>
+                        <th scope="col">produto</th>
+                        <th scope="col">loja</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {productstoreList.map((productstore, index) =>
+                        <ProductStoreRow key={index} productstore={productstore}/>
+                      )}
+                    </tbody>
+                  </Table>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      )
+    }
+}
+  
+export default ProductsStores;

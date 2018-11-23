6f6c88e2b4b8d8e195a591cfbd64616370e09d75
Luis Felipe <luisfelipegodoi@live.com>
2018-11-23 12:54:23 -0200

    Add cors.js for enable call in externals apps.

---
+++ api/cors.js
@@ -0,0 +1,12 @@
+module.exports = function (req, res, next) {
+    res.header('Content-Type', 'application/json; charset=utf-8');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, user.login, User-Agent, user.password, skiploading, skiperror, token");
+    res.header("Access-Control-Allow-Methods", "POST,PUT,DELETE,GET,OPTIONS");
+
+    if (req.method === 'OPTIONS') {
+        res.status(200).send();
+        return;
+    }
+    next();
+};

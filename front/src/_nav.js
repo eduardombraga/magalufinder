0ed46455817d0c2565bf738036bdd65be53da027
Luis Felipe Godói <luisfelipegodoi@live.com>
2018-11-22 23:24:17 -0200

    Remove unecessary node in menu.

--- front/src/_nav.js
+++ front/src/_nav.js
@@ -11,20 +11,6 @@ export default {
     },
     {
       title: true,
-      name: 'Theme',
-      wrapper: {            // optional wrapper object
-        element: '',        // required valid HTML5 element tag
-        attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
-      },
-      class: ''             // optional class names space delimited list for title item ex: "text-center"
-    },
-    {
-      name: 'Colors',
-      url: '/theme/colors',
-      icon: 'icon-drop',
-    },
-    {
-      title: true,
       name: 'Components',
       wrapper: {
         element: '',

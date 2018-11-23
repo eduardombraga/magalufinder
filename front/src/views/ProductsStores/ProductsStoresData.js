490c7e86afb6367e30a2252bd1b5ec3044e371c6
Luis Felipe <luisfelipegodoi@live.com>
2018-11-23 10:11:00 -0200

    Create a productsStoresData file for populate.

---
+++ front/src/views/ProductsStores/ProductsStoresData.js
@@ -0,0 +1,15 @@
+const productsstoresData = [
+    {productid: 1, storeid: 1},
+    {productid: 1, storeid: 48},
+    {productid: 1, storeid: 3},
+    {productid: 2, storeid: 1},
+    {productid: 2, storeid: 4},
+    {productid: 3, storeid: 48},
+    {productid: 4, storeid: 1},
+    {productid: 4, storeid: 4},
+    {productid: 4, storeid: 3},
+    {productid: 5, storeid: 1},
+    {productid: 5, storeid: 48},
+  ]
+  
+  export default productsstoresData

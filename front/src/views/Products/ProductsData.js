cfbf95baca5749df5f4dfdb813677bab4797eed6
Luis Felipe Godói <luisfelipegodoi@live.com>
2018-11-23 03:06:07 -0200

    Populate a productsdata file with data.

---
+++ front/src/views/Products/ProductsData.js
@@ -0,0 +1,8 @@
+const productsData = [
+    {id: 1, productname: 'SmartTv Samsung', productvalue: 1000, description: 'SmartTv Samsung 4k 50 polegadas'},
+    {id: 2, productname: 'Iphone 8', productvalue: 3000, description: 'Iphone 8 ultima geracao'},
+    {id: 3, productname: 'Caixa jbl extreme', productvalue: 800, description: 'jbl extreme bombox 8 horas bateria'},
+    {id: 4, productname: 'Dvd pionner', productvalue: 1500, description: 'Dvd automotivo pionner 7 polegadas 500 w'},
+  ]
+  
+  export default productsData

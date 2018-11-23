e1b343a575a57a15c7c6065a04a2693118ee4649
Luis Felipe <luisfelipegodoi@live.com>
2018-11-23 09:53:05 -0200

    Create a StoreRow function.

--- front/src/views/Stores/Stores.js
+++ front/src/views/Stores/Stores.js
@@ -2,3 +2,16 @@ import React, { Component } from 'react';
 import { Badge, Card, CardBody, CardHeader, Col, Row, Table } from 'reactstrap';
 
 import storesData from './StoresData'
+
+function StoreRow(props) {
+    const store = props.store
+    const storeLink = `#/stores/${store.storeid}`
+  
+    return (
+      <tr key={store.storeid.toString()}>
+          <th scope="row"><a href={storeLink}>{store.storeid}</a></th>
+          <td><a href={storeLink}>{store.cep}</a></td>
+          <td>{store.description}</td>
+      </tr>
+    )
+  }

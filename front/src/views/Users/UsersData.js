555ff225d276926d9e2eb7f2652808a78f021076
Luis Felipe Godói <luisfelipegodoi@live.com>
2018-11-22 23:01:35 -0200

    Remove unecessary users for perform tests.

--- front/src/views/Users/UsersData.js
+++ front/src/views/Users/UsersData.js
@@ -1,9 +1,6 @@
 const usersData = [
-  {id: 1, name: 'Samppa Nori', registered: 'sim'},
-  {id: 2, name: 'Estavan Lykos', registered: 'não'},
-  {id: 3, name: 'Chetan Mohamed', registered: 'não'},
-  {id: 4, name: 'Derick Maximinus', registered: 'sim'},
-  {id: 5, name: 'Friderik Dávid', registered: 'não'}
+  {id: 1, name: 'admin', registered: 'sim'},
+  {id: 2, name: 'cliente', registered: 'não'}
 ]
 
 export default usersData

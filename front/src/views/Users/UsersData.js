cfc9886a7e16011d7dadb73e00d5d3822e755b43
Luis Felipe Godói <luisfelipegodoi@live.com>
2018-11-22 22:46:05 -0200

    Add CoreUI template for React.

---
+++ front/src/views/Users/UsersData.js
@@ -0,0 +1,9 @@
+const usersData = [
+  {id: 1, name: 'Samppa Nori', registered: 'sim'},
+  {id: 2, name: 'Estavan Lykos', registered: 'não'},
+  {id: 3, name: 'Chetan Mohamed', registered: 'não'},
+  {id: 4, name: 'Derick Maximinus', registered: 'sim'},
+  {id: 5, name: 'Friderik Dávid', registered: 'não'}
+]
+
+export default usersData

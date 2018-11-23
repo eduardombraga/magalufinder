00a69f937138d57c9352544a6471c105d0957425
Luis Felipe Godói <luisfelipegodoi@live.com>
2018-11-23 03:11:29 -0200

    create a function productRow.

--- front/src/views/Products/Products.js
+++ front/src/views/Products/Products.js
@@ -2,3 +2,17 @@ import React, { Component } from 'react';
 import { Badge, Card, CardBody, CardHeader, Col, Row, Table } from 'reactstrap';
 
 import productsData from './productsData'
+
+function ProductRow(props) {
+    const product = props.product
+    const productLink = `#/products/${product.id}`
+  
+    return (
+      <tr key={product.id.toString()}>
+          <th scope="row"><a href={productLink}>{product.id}</a></th>
+          <td><a href={productLink}>{product.productname}</a></td>
+          <td>{product.productvalue}</td>
+          <td>{product.description}</td>
+      </tr>
+    )
+  }

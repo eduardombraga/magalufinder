e7c230b344e1c6014b23e6bf63a6e0373b1e6e81
Luis Felipe Godói <luisfelipegodoi@live.com>
2018-11-24 13:09:33 -0200

    Prepare a controller to access google geo

--- api/controllers/FindNearProducts.js
+++ api/controllers/FindNearProducts.js
@@ -1,6 +1,14 @@
 'use strict';
 
 var Boom = require('boom');
+var req = require('request');
+
+var options = {
+    method: 'GET',
+    json: true
+}
+var originCep = '14403-363';
+var destinationCep = '14010-060';
 
 function FindNearProductsController() {
 };
@@ -8,7 +16,15 @@ function FindNearProductsController() {
 // [GET] /findnearproducts
 FindNearProductsController.prototype.index = function(request, reply) {
 
-    reply('FindNearProducts');
+    try {
+        req('https://maps.googleapis.com/maps/api/distancematrix/json?origins='+originCep+'&destinations='+destinationCep+'&key='+process.env.GMAPS_TOKEN, function (error, response, body) {
+            reply(body);
+        });
+    } catch (error) {
+        reply(Boom.badRequest(error.message));
+    }
+
+    //reply('FindNearProducts');
 };
 
 module.exports = FindNearProductsController;

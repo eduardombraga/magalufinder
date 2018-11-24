af23f46b54eb9d6e296fc97f41b61b6816c1b03f
Luis Felipe Godói <luisfelipegodoi@live.com>
2018-11-24 02:44:47 -0200

    Create a login route.

---
+++ api/routes/login.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// Login routes
+var Joi = require('joi');
+var LoginController = require('../controllers/Login');
+
+exports.register = function(server, options, next) {
+    // Setup the controller
+    var loginController = new LoginController(options.database);
+
+    // Binds all methods
+    // similar to doing `loginController.index.bind(loginController);`
+    // when declaring handlers
+    server.bind(loginController);
+
+    // Declare routes
+    server.route([
+        {
+            method: 'GET',
+            path: '/login',
+            config: {
+                handler: loginController.index,
+                validate: {
+                    
+                }
+            }
+        }
+    ]);
+
+    next();
+}
+
+exports.register.attributes = {
+    name: 'routes-login',
+    version: '1.0.1'
+};

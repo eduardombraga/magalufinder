65d695d0c63ad8e1a550477d559a4ff4f4dc2ec5
Luis Felipe Godói <luisfelipegodoi@live.com>
2018-11-24 02:19:47 -0200

    Remove unecessary code.

--- api/controllers/FindNearProducts.js
+++ api/controllers/FindNearProducts.js
@@ -4,17 +4,7 @@ var Boom = require('boom');
 
 // [GET] /findnearproducts
 FindNearProductsController.prototype.index = function(request, reply) {
-    var start = request.query.start;
-    var limit = request.query.limit;
-
-    if (start == null) {
-        start = 0
-    }
-
-    if (limit == null) {
-        limit = start + 9
-    }
-
+    
     reply('FindNearProducts');
 };
 

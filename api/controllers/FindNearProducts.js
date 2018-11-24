d3f25979dcdc39a307c7e63564ca25b7d682224d
Luis Felipe Godói <luisfelipegodoi@live.com>
2018-11-24 13:22:58 -0200

    Modify request in controller.

--- api/controllers/FindNearProducts.js
+++ api/controllers/FindNearProducts.js
@@ -17,14 +17,12 @@ function FindNearProductsController() {
 FindNearProductsController.prototype.index = function(request, reply) {
 
     try {
-        req('https://maps.googleapis.com/maps/api/distancematrix/json?origins='+originCep+'&destinations='+destinationCep+'&key='+process.env.GMAPS_TOKEN, function (error, response, body) {
+        req('https://maps.googleapis.com/maps/api/distancematrix/json?origins='+request.params.originCep+'&destinations='+request.params.destinationCep+'&key='+process.env.GMAPS_TOKEN, function (error, response, body) {
             reply(body);
         });
     } catch (error) {
         reply(Boom.badRequest(error.message));
     }
-
-    //reply('FindNearProducts');
 };
 
 module.exports = FindNearProductsController;

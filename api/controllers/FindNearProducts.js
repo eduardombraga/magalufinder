0724f5f805518c8cc2e4ea74b9ed07a156d7e0b1
Luis Felipe Godói <luisfelipegodoi@live.com>
2018-11-24 13:50:47 -0200

    Format to return in json.

--- api/controllers/FindNearProducts.js
+++ api/controllers/FindNearProducts.js
@@ -10,8 +10,8 @@ function FindNearProductsController() {
 FindNearProductsController.prototype.index = function(request, reply) {
 
     try {
-        req('https://maps.googleapis.com/maps/api/distancematrix/json?origins='+request.params.originCep+'&destinations='+request.params.destinationCep+'&key='+process.env.GMAPS_TOKEN, function (error, response, body) {
-            reply(body);
+        req('https://maps.googleapis.com/maps/api/distancematrix/json?origins='+request.params.originCep+'&destinations='+request.params.destinationCep+'&key='+process.env.GMAPS_TOKEN, function (error, response, body) {    
+        reply(JSON.parse(body));
         });
     } catch (error) {
         reply(Boom.badRequest(error.message));

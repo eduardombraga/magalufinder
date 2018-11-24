cf1e6169c1713de62ea88b8f0cbff12895adf3d6
Luis Felipe Godói <luisfelipegodoi@live.com>
2018-11-24 03:09:11 -0200

    Add a knex file with connection inf

---
+++ api/knex.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    client: 'mysql',
+      connection: {
+          host: '127.0.0.1',
+          user: 'root',
+          password: 'E=mc21988',
+          database: 'magalufinder'
+      }
+
+};

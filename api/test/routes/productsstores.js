b92566c0f2cb52f3d6b3340d9c987697071b33cd
Luis Felipe Godói <luisfelipegodoi@live.com>
2018-11-27 06:06:36 -0200

    better code tests in productsstores.

--- api/test/routes/productsstores.js
+++ api/test/routes/productsstores.js
@@ -36,7 +36,29 @@ describe('Routes /productsstores', function() {
 
     describe('POST /productsstores', function() {
 
+        it('fails when there\'s no payload', function(done) {
+            var options = {method: 'POST', url: '/productsstores'};
+            server.inject(options, function(response) {
+                response.statusCode.should.be.exactly(400);
+                done();
+            });
+        });
 
+        it('fails with an invalid payload', function(done) {
+            var options = {method: 'POST', url: '/productsstores', payload: {}};
+            server.inject(options, function(response) {
+                response.statusCode.should.be.exactly(400);
+                done();
+            });
+        });
+
+        it('fails when there\'s too many properties in the payload', function(done) {
+            var options = {method: 'POST', url: '/productsstores', payload: {productid: 'a product id', something: 'else'}};
+            server.inject(options, function(response) {
+                response.statusCode.should.be.exactly(400);
+                done();
+            });
+        });
 
     });
 

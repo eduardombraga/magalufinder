147cf9ee7068876f0279a0a029770efe7e7adf3e
Luis Felipe Godói <luisfelipegodoi@live.com>
2018-11-27 04:56:28 -0200

    Add unit tests for stores.

---
+++ api/test/routes/stores.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var lab = exports.lab = require('lab').script();
+var describe = lab.describe;
+var it = lab.it;
+var beforeEach = lab.beforeEach;
+var afterEach = lab.afterEach;
+var server = require('../../');
+var db = server.database;
+
+describe('Routes /stores', function() {
+
+    describe('GET /stores', function() {
+
+
+
+    });
+
+    describe('GET /stores/{id}', function() {
+
+        
+
+    });
+
+    describe('POST /stores', function() {
+
+
+
+    });
+
+    describe('PUT /stores/{id}', function() {
+
+        
+
+    });
+
+    describe('DELETE /stores/{id}', function() {
+
+        
+
+    });
+
+});

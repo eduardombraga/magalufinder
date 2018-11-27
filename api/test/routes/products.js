9ce713dd32ee4c4e4c6eee4bfd13710e5ce2bb48
Luis Felipe Godói <luisfelipegodoi@live.com>
2018-11-27 06:27:25 -0200

    Tests in delete verb.

--- api/test/routes/products.js
+++ api/test/routes/products.js
@@ -76,7 +76,21 @@ describe('Routes /products', function() {
 
     describe('DELETE /products/{id}', function() {
 
-        
+        it('validates id in url parameter', function(done) {
+            var options = {method: 'DELETE', url: '/products/1'};
+            server.inject(options, function(response) {
+                response.statusCode.should.be.exactly(400);
+                done();
+            });
+        });
+
+        it('returns 404 when task isn\'t found', function(done) {
+            var options = {method: 'DELETE', url: '/products/123321555'};
+            server.inject(options, function(response) {
+                response.statusCode.should.be.exactly(404);
+                done();
+            });
+        });
 
     });
 

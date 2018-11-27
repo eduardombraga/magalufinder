d123efec3d9872e5f7123bc939d9d3da23e09d8c
Luis Felipe Godói <luisfelipegodoi@live.com>
2018-11-27 04:53:50 -0200

    Add unit tests for users.

---
+++ api/test/routes/users.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var lab = exports.lab = require('lab').script();
+var describe = lab.describe;
+var it = lab.it;
+var beforeEach = lab.beforeEach;
+var afterEach = lab.afterEach;
+var server = require('../../');
+var db = server.database;
+
+describe('Routes /users', function() {
+
+    describe('GET /users', function() {
+
+
+
+    });
+
+    describe('GET /users/{id}', function() {
+
+        
+
+    });
+
+    describe('POST /users', function() {
+
+
+
+    });
+
+    describe('PUT /users/{id}', function() {
+
+        
+
+    });
+
+    describe('DELETE /users/{id}', function() {
+
+        
+
+    });
+
+});

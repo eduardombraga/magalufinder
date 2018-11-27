a344b2a38bf590fd26964950891f1df9bb986566
Luis Felipe Godói <luisfelipegodoi@live.com>
2018-11-27 05:57:11 -0200

    Add tests in productsstores.

--- api/test/routes/productsstores.js
+++ api/test/routes/productsstores.js
@@ -12,13 +12,25 @@ describe('Routes /productsstores', function() {
 
     describe('GET /productsstores', function() {
 
-
+        it('returns 200 HTTP status code', function(done) {
+            var options = {method: 'GET', url: '/productsstores'};
+            server.inject(options, function(response) {
+                response.statusCode.should.be.exactly(200);
+                done();
+            });
+        });
 
     });
 
     describe('GET /productsstores/{id}', function() {
 
-        
+        it('validates id in url parameter', function(done) {
+            var options = {method: 'GET', url: '/productsstores/1'};
+            server.inject(options, function(response) {
+                response.statusCode.should.be.exactly(400);
+                done();
+            });
+        });
 
     });
 

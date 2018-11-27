faf0a456adebdb554964534f0cf068c4ed724072
Luis Felipe Godói <luisfelipegodoi@live.com>
2018-11-27 05:31:11 -0200

    Init unit tests.

--- api/test/routes/users.js
+++ api/test/routes/users.js
@@ -12,13 +12,25 @@ describe('Routes /users', function() {
 
     describe('GET /users', function() {
 
-
+        it('returns 200 HTTP status code', function(done) {
+            var options = {method: 'GET', url: '/users'};
+            server.inject(options, function(response) {
+                response.statusCode.should.be.exactly(200);
+                done();
+            });
+        });
 
     });
 
     describe('GET /users/{id}', function() {
 
-        
+        it('validates id in url parameter', function(done) {
+            var options = {method: 'GET', url: '/users/1'};
+            server.inject(options, function(response) {
+                response.statusCode.should.be.exactly(400);
+                done();
+            });
+        });
 
     });
 

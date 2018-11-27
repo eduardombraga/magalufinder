4441384de50295dfaf14eb00630f5bce7f4c5660
Luis Felipe Godói <luisfelipegodoi@live.com>
2018-11-27 05:48:22 -0200

    Add tests in products.

--- api/test/routes/products.js
+++ api/test/routes/products.js
@@ -12,13 +12,25 @@ describe('Routes /products', function() {
 
     describe('GET /products', function() {
 
-
+        it('returns 200 HTTP status code', function(done) {
+            var options = {method: 'GET', url: '/products'};
+            server.inject(options, function(response) {
+                response.statusCode.should.be.exactly(200);
+                done();
+            });
+        });
 
     });
 
     describe('GET /products/{id}', function() {
 
-        
+        it('validates id in url parameter', function(done) {
+            var options = {method: 'GET', url: '/products/1'};
+            server.inject(options, function(response) {
+                response.statusCode.should.be.exactly(400);
+                done();
+            });
+        });
 
     });
 
